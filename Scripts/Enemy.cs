768fd06a2359aed737b3fa77db2978fd2fb121a1
dosulu <eray.ulu891@gmail.com>
2025-11-10 12:39:37 +0300

    + hornet added

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -3,14 +3,14 @@ using System;
 
 public class Enemy : KinematicBody2D
 {
-    [Export] public int Speed = 100;
-    private Vector2 _velocity = Vector2.Zero;
+	[Export] public int Speed = 100;
+	private Vector2 _velocity = Vector2.Zero;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        var player = GetNode<Player>("/root/Level1/Player");
-        var direction = (player.Position - Position).Normalized();
-        _velocity = direction * Speed;
-        MoveAndSlide(_velocity);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		var player = GetNode<Player>("/root/Level1/Player");
+		var direction = (player.Position - Position).Normalized();
+		_velocity = direction * Speed;
+		MoveAndSlide(_velocity);
+	}
 }
